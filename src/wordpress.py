91f876da723e305f28ee3c7750c0c7de5e3ba495
Taiane Linhares <taianeclinhares@gmail.com>
2021-08-17 09:50:04 +0200

    logger to print jinja2 env object

--- src/wordpress.py
+++ src/wordpress.py
@@ -64,6 +64,7 @@ def wp_message(post_day, name, wiki, synonyms, men, year, countries, img, imgsrc
     logger.info("After Jinja2 FileSystemLoader.")
     env = Environment(loader=file_loader)
     logger.info("Before environment get_template.")
+    logger.info(env)
     template = env.get_template("wp_template.html")
     logger.info("After environment get_template.")
     content_text = template.render(
