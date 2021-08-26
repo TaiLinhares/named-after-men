15edef6e75a72150738580a4d82e9c880b21bf24
Taiane Linhares <taianeclinhares@gmail.com>
2021-08-26 17:27:16 +0200

    start writing check_dtype and test it

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 import requests
-from utils import text_concat
+from utils import text_concat, check_dtype
 
 class TestUtils(unittest.TestCase):
 
@@ -10,4 +10,5 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(text_concat(["A", "B"], "<foo>", "</foo>"),"<foo>A</foo>, and <foo>B</foo>")
         self.assertEqual(text_concat(["A", "B", "C"], "<foo>", "</foo>"),"<foo>A</foo>, <foo>B</foo>, and <foo>C</foo>")
 
-    # def test_get_pic(self):
+    def test_check_dtype(self):
+        self.assertEqual(check_dtype(("dog", int(1), ["a", "b", "c"]), ["str", "int", "list"])[0],True)
